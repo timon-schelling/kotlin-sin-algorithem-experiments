cf449ab2cadd792ff803c789c163895192b099dc
TIMON-WORKSTATI\timon <timon.schelling@outlook.de>
2020-03-01 18:43:42 +0100

    -

--- main.kts
+++ main.kts
@@ -10,9 +10,9 @@ fun sin(a: Double): Result {
     x = x % (2 * Math.PI)
 
     // compute the Taylor series approximation
-    var term = 1.0 // ith term = x^i / i!
+    var term = 1.0 // ith term = x^n / n!
 
-    var sum = 0.0 // sum of first i terms in taylor series
+    var sum = 0.0 // sum of first n terms in taylor series
 
     var n = 1
     while (term != 0.0) {
